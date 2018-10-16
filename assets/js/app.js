3aba079e1dafcf9f1a0e3d193a0c0ee111c772a7
iltruma <casini.cosimo@gmail.com>
2018-10-17 00:18:08 +0200

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    /*
+    var template = '{{#data}}<div><h2>{{Title}}</h2><p>{{Course}}</p><p>{{Category}}</p></div>{{/data}}';
+    var data = JSON.parse('[{"Title":"Algorithms","Course":"CSI241","Category":"science"},{"Title":"Fluid dynamics","Course":"PHY345","Category":"science"}]')
+    var html = Mustache.to_html(template, { data: data });
+    */
+
+    var btnCancel = $('<button></button>').text('Cancel').addClass('btn btn-danger').on('click', function(){
+        $('#smartwizard').smartWizard("reset");
+        $('#myForm').find("input, textarea").val("");
+    });
+
+    // Smart Wizard
+    $('#smartwizard').smartWizard({
+            selected: 0,
+            theme: 'arrows',
+            transitionEffect:'fade',
+            toolbarSettings: {toolbarPosition: 'bottom',
+                              toolbarExtraButtons: [btnCancel]
+                            },
+            anchorSettings: {
+                        markDoneStep: true, // add done css
+                        markAllPreviousStepsAsDone: true, // When a step selected by url hash, all previous steps are marked done
+                        removeDoneStepOnNavigateBack: true, // While navigate back done step after active step will be cleared
+                        enableAnchorOnDoneStep: true // Enable/Disable the done steps navigation
+                    }
+    });
+
+    $("#smartwizard").on("showStep", function(e, anchorObject, stepNumber, stepDirection) {
+        if(stepNumber == 3){
+          alert("aaah");
+          $.get('templates/hcdwp.txt', function(templates) {
+              var data = JSON.parse('[{\"numero\":\"' + $("numero").value + '\",' +
+                                      '\"categoria\":\"' + $("categoria").value + '\",' +
+                                      '\"titolo\":\"' + $("titolo").value + '\",' +
+                                      '\"messaggio\":\"' + $("messaggio").value + '\",' +
+                                      '\"versioni\":\"' + $("versioni").value + '\",' +
+                                      + '}]');
+              $('risultato').innerHTML = Mustache.to_html(template, { data: data });
+          });
+        }
+    });
+
+});
